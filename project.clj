8907453e257dc85b4498d5ae67d9fd29a9699569
Ziyang Hu <hu.ziyang@cantab.net>
2022-11-06 16:29:07 +0800

    add signing key

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://github.com/cozodb/cozo"
   :license {:name "MIT/Apache-2.0/BSD-3-Clause"}
   :dependencies []
+  :signing {:gpg-key "hu.ziyang@cantab.net"}
   :java-source-paths ["java"])
